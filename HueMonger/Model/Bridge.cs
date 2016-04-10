604512364e0d190ec6bbd3c2112fdfc7542536f6
mjdavy <mjdavy@hotmail.com>
2016-04-10 09:16:48 -0400

    Initial commit

---
+++ HueMonger/Model/Bridge.cs
@@ -0,0 +1,30 @@
+﻿using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HueMonger.Model
+{
+    public class Bridge
+    {
+        public static async Task<IEnumerable<string>> Initialize()
+        {
+            IBridgeLocator locator = new HttpBridgeLocator();
+
+            //For Windows 8 and .NET45 projects you can use the SSDPBridgeLocator which actually scans your network. 
+            //See the included BridgeDiscoveryTests and the specific .NET and .WinRT projects
+            IEnumerable<string> bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+            return bridgeIPs;
+        }
+
+        public static async Task<string> Register(string ip)
+        {
+            ILocalHueClient client = new LocalHueClient(ip);
+            var appKey = await client.RegisterAsync("huemonger", "mjdavy@hotmail.com");
+            return appKey;
+        }
+    }
+}
---
+++ HueMonger/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,39 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HueMonger.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        static ViewModelLocator()
+        {
+            RegisterViewModels();
+            RegisterMessages();
+        }
+
+        static void RegisterViewModels()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        static void RegisterMessages()
+        {
+            //var main = ServiceLocator.Current.GetInstance<MainViewModel>();
+            //Messenger.Default.Register<SearchMessage>(main, 1, main.OnSearch);
+        }
+    }
+}
