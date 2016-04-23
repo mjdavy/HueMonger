b8779c1d98940cfc5d37f51992c1c8bf4b17e5d7
mjdavy <mjdavy@hotmail.com>
2016-04-23 09:32:22 -0400

    Added light viewmodel

--- HueMonger/ViewModel/MainViewModel.cs
+++ HueMonger/ViewModel/MainViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using GalaSoft.MvvmLight;
+using HueMonger.Model;
+using Q42.HueApi;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +12,23 @@ namespace HueMonger.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            Init();
+        }
+
+        public ObservableCollection<Light> Lights
+        {
+            get;
+            set;
+        }
+            
+        public async void Init()
+        {
+            var bridges = await Bridge.Initialize();
+            var myBridge = bridges.First();
+            var key = await Bridge.Register(myBridge);
+            
+        }
     }
 }
